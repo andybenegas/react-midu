d0ed882d104c984f95a0dae0cb3acea9ebb9b136
Andres <andres212_mza89@hotmail.com>
2024-07-18 19:21:39 -0300

    Save to LocalStorage

--- projects/02-ta-te-ti/src/App.jsx
+++ projects/02-ta-te-ti/src/App.jsx
@@ -4,17 +4,28 @@ import { Square } from "./components/Square";
 import { TURNS } from "./constants/constants";
 import { checkEndGame, checkWinnerFrom } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
+import { resetGameStorage, saveGameToStorage } from "./logic/storage";
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  });
+  
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  });
+
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -27,6 +38,8 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
+    saveGameToStorage({board:newBoard, turn: newTurn});
+    
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti();
